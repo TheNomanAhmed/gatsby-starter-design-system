de91f746caf71e8b1cad63d801b68033a852df5a
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-01-27 15:09:46 -0600

    Adds global styles injection for css reset

--- src/components/styles/styles.js
+++ src/components/styles/styles.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${props => (props.whiteColor ? 'white' : 'black')};
+  }
+`
 
 export const Section = styled.section`
-    height: 600px;
-    width: auto;
+  height: 600px;
+  width: auto;
 `
